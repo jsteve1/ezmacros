c93810f3a6ffd05a66b5bcb26a6798c58025f657
Jacob Stevens <jakestevens082@gmail.com>
2025-02-15 21:19:00 -0800

    fix base url in vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/ezmacrosbruh/', // Replace with your repository name
+  base: '/ezmacros/', // Repository name
   build: {
     outDir: 'dist',
     sourcemap: true
